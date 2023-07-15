d06409d07afba795a5423446ecf898a196f0ed52
George Claridge <glgclaridge@gmail.com>
2023-07-15 16:35:52 +1200

    Fixed Links

--- my-project/src/mobilecomponents/navbar/Navbar.jsx
+++ my-project/src/mobilecomponents/navbar/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
 
       <div className="flex justify-between self-center text-xl font-bold bg-white border-[2px] rounded-full py-4 px-5">
         <a target='_blank'rel='noopener noreferrer' href="https://github.com/GeorgeClaridge/BrutalDesign"><img src={Github} alt="Github" className='self-center w-10 mr-2'/></a>
-        <a target='_blank'rel='noopener noreferrer' href="https://github.com/GeorgeClaridge/BrutalDesign"><img src={Linkedin} alt="Linkedin" className='self-center w-10 mr-2'/></a>
+        <a target='_blank'rel='noopener noreferrer' href="https://www.linkedin.com/in/george-claridge-6a5a1721b/"><img src={Linkedin} alt="Linkedin" className='self-center w-10 mr-2'/></a>
         <a target='_blank'rel='noopener noreferrer' href="https://www.figma.com/file/n9Y2ZU6f0YmERYSrgWiBIt/Brutal?type=design&node-id=0%3A1&mode=design&t=8zFX6ZBIMMG0o9oC-1"><img src={Figma} alt="Figma" className='Icon self-center w-10'/></a>
       </div>
 
