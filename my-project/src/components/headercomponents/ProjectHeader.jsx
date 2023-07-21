6136294a7894511ed624ca1c31d1f73b2d46d791
George Claridge <glgclaridge@gmail.com>
2023-07-21 12:09:56 +1200

    Project Tab Template

--- my-project/src/components/headercomponents/ProjectHeader.jsx
+++ my-project/src/components/headercomponents/ProjectHeader.jsx
@@ -2,25 +2,57 @@ const ProjectHeader = () => {
 
   return (
     <div>
-      <div className="HeaderContainer flex justify-between">
+      <div className="HeaderContainer flex justify-center">
+        <div className="Header text-6xl font-bold">PROJECTS</div>
+      </div>
 
-        <div className="Left">
-          <div className="Header text-6xl font-bold">Project</div>
-          <div className="SubHeader text-purple text-3xl font-bold pt-2">Subheader</div>
-        </div>
+      <div className="flex justify-between pt-3">
+          <div className="w-[30%]">
+            <div className="bg-purple rounded-t-[20px] border-[2px] flex flex-row justify-center py-2">
+              <div className="center text-2xl font-semibold">PROJECT NAME</div>
+            </div>
 
-        <div className="Right self-center">
-          Test
-        </div>
+            <div className="Content rounded-b-[20px] border-[2px] border-t-0 p-5">
+              <div className="Header text-2xl text-purple">Tech</div>
+
+              <div className="Text pt-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</div>
+
+              <a target='_blank'rel='noopener noreferrer' href="https://github.com/GeorgeClaridge?tab=repositories"><button className="rounded-lg bg-green border-[2px] shadow-solid-primary px-3 py-2 mt-3 mb-4 text-base hover:-translate-y-1 hover:scale-110 duration-300">All Repositorys</button></a>
+            </div>
+          </div>
+
+          <div className="w-[30%]">
+            <div className="bg-purple rounded-t-[20px] border-[2px] flex flex-row justify-center py-2">
+              <div className="center text-2xl font-semibold">PROJECT NAME</div>
+            </div>
+
+            <div className="Content rounded-b-[20px] border-[2px] border-t-0 p-5">
+              <div className="Header text-2xl text-purple">Tech</div>
+
+              <div className="Text pt-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</div>
+
+              <a target='_blank'rel='noopener noreferrer' href="https://github.com/GeorgeClaridge?tab=repositories"><button className="rounded-lg bg-green border-[2px] shadow-solid-primary px-3 py-2 mt-3 mb-4 text-base hover:-translate-y-1 hover:scale-110 duration-300">All Repositorys</button></a>
+            </div>
+          </div>
+
+          <div className="w-[30%]">
+            <div className="bg-purple rounded-t-[20px] border-[2px] flex flex-row justify-center py-2">
+              <div className="center text-2xl font-semibold">PROJECT NAME</div>
+            </div>
+
+            <div className="Content rounded-b-[20px] border-[2px] border-t-0 p-5">
+              <div className="Header text-2xl text-purple">Tech</div>
+
+              <div className="Text pt-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</div>
+
+              <a target='_blank'rel='noopener noreferrer' href="https://github.com/GeorgeClaridge?tab=repositories"><button className="rounded-lg bg-green border-[2px] shadow-solid-primary px-3 py-2 mt-3 mb-4 text-base hover:-translate-y-1 hover:scale-110 duration-300">All Repositorys</button></a>
+            </div>
+          </div>
 
       </div>
 
-      <div className="text text-xl font-bold font-IBMsans pt-3">CONTENT</div>
-      <div className="text text-xl font-bold font-IBMsans pt-3">CONTENT</div>
-      <div className="text text-xl font-bold font-IBMsans pt-3">CONTENT</div>
-      <div className="text text-xl font-bold font-IBMsans pt-3">CONTENT</div>
-      <div className="text text-xl font-bold font-IBMsans pt-3">CONTENT</div>
-      <div className="text text-xl font-bold font-IBMsans pt-3">CONTENT</div>
+      
+
 
       <div className="Line border-b-[2px] pt-6"></div>
     </div>
