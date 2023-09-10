ff06a1bf6194cf4be00d20211353b04643e5b479
George Claridge <glgclaridge@gmail.com>
2023-09-10 13:58:13 +1200

    WIP Image Added

--- my-project/src/components/headercomponents/HomeHeader.jsx
+++ my-project/src/components/headercomponents/HomeHeader.jsx
@@ -1,10 +1,12 @@
+import {Construct} from './imports';
+
 const HomeHeader = () => {
 
   return (
     <div>
       <div className="HeaderContainer">
 
-        <div className="Header text-6xl font-bold flex justify-start">
+        <div className="Header text-6xl font-bold flex justify-center">
           <div className='hover:-translate-y-2 duration-[400ms] hover:text-green hover:scale-110'>
             G
           </div>
@@ -48,15 +50,17 @@ const HomeHeader = () => {
             E
           </div>
         </div>
-        <div className="SubHeader text-purple text-3xl font-bold pt-2">Neubrutalism</div>
+
+        <div className="SubHeader text-purple text-3xl font-bold pt-2 text-center">Neubrutalism</div>
+
+        <div className="flex justify-center">
+          <img src={Construct} alt="Home" className='Icon self-center w-[30%] pt-5'/>
+        </div>
 
       </div>
 
-      <div className="text text-xl font-bold font-IBMsans pt-3">Hey I’m George Claridge, a website developer located in Christchurch, New Zealand. I have just finished my diploma in IT Support and I'm currently looking for work. If you have any opportunities please contact me on Linkedin or on email at glgclaridge@gmail.com</div>
-      <div className="text text-xl font-bold font-IBMsans pt-3">This website was made for me to learn about Neubrutalism website design and making a window that changes based on a user’s decision. In the future I want to build a website based on a PC’s Desktop that can change based on a user’s decisions. This could include a taskbar, background images and icon styles. This website is currently a work in progress and will be gaining new features as I learn.</div>
-      <div className="text text-xl font-bold font-IBMsans pt-3">I became interested in Neubrutalism when looking at multiple website developer’s portfolios and noticing common themes that I found out were from the Neubrutalism style. I used these websites and designs as references and took elements I liked to create this website using Figma and React.</div>
-      <div className="text text-xl font-bold font-IBMsans pt-3">To get the base template of the website I created a couple concepts using Figma’s figjam board to sketch the concepts. I then brought it into a design file and started developing it further by making decisions on color pallet, icons and responsive design. Once completing the design I built it using React with Vite, Tailwind for CSS and Github for version control. To host the website i'm using Hostinger which allows me to use tools that help me improve performance.</div>
-      <div className="Line border-b-[2px] pt-6"></div>
+      
+
     </div>
   )
 }
--- my-project/src/components/headercomponents/imports.js
+++ my-project/src/components/headercomponents/imports.js
@@ -1,7 +1,9 @@
 import Github from '../../assets/Github.svg'
 import Linkedin from '../../assets/Linkedin.svg'
+import Construct from '../../assets/Construct.png'
 
 export {
     Github,
     Linkedin,
+    Construct,
 }
